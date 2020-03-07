0cacc82252e869520f10ddad1399680cf7cf19e0
wenji <liuwj@shanghaitech.edu.cn>
2020-03-07 09:34:33 +0800

    first upload

---
+++ Opengl_cmake_template/opengl-basic-windows/3rdLibs/glfw-3.3.2/build/tests/CMakeFiles/triangle-vulkan.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/triangle-vulkan.dir/triangle-vulkan.c.obj"
+  "CMakeFiles/triangle-vulkan.dir/__/deps/glad_vulkan.c.obj"
+  "triangle-vulkan.pdb"
+  "triangle-vulkan.exe"
+  "triangle-vulkan.exe.manifest"
+  "libtriangle-vulkan.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/triangle-vulkan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
